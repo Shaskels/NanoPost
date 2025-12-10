0f8f547d6d656f5c639d1d7104626b9fc47a7ab9
Shaskels <allasidner028@gmail.com>
2025-12-10 20:56:10 +0700

    pull to refresh

--- app/src/main/java/com/example/nanopost/presentation/imagesScreen/ImagesScreen.kt
+++ app/src/main/java/com/example/nanopost/presentation/imagesScreen/ImagesScreen.kt
@@ -2,6 +2,7 @@ package com.example.nanopost.presentation.imagesScreen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -9,12 +10,15 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import coil3.compose.AsyncImage
@@ -29,6 +33,7 @@ fun ImagesScreen(
     onImageClick: (String) -> Unit
 ) {
     val images = imagesViewModel.images.collectAsLazyPagingItems()
+    val pullToRefreshState = rememberPullToRefreshState()
 
     Scaffold(
         topBar = {
@@ -45,34 +50,40 @@ fun ImagesScreen(
             )
         }
     ) { paddingValues ->
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            verticalArrangement = Arrangement.spacedBy(2.dp),
-            horizontalArrangement = Arrangement.spacedBy(2.dp),
-            contentPadding = paddingValues,
-            modifier = Modifier.padding(horizontal = 16.dp)
+        PullToRefreshBox(
+            isRefreshing = images.loadState.refresh is LoadState.Loading,
+            onRefresh = images::refresh,
+            state = pullToRefreshState,
+            modifier = Modifier.fillMaxSize()
         ) {
-            items(
-                count = images.itemCount,
-                key = images.itemKey { it.id }
-            ) { index ->
-                val item = images[index]
-                if (item != null) {
-                    AsyncImage(
-                        model = item.sizes.first().url,
-                        contentDescription = null,
-                        contentScale = ContentScale.FillBounds,
-                        placeholder = painterResource(R.drawable.no_photo),
-                        modifier = Modifier
-                            .height(125.dp)
-                            .clickable(onClick = { onImageClick(item.id) })
-                    )
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                verticalArrangement = Arrangement.spacedBy(2.dp),
+                horizontalArrangement = Arrangement.spacedBy(2.dp),
+                contentPadding = paddingValues,
+                modifier = Modifier.padding(horizontal = 16.dp).fillMaxSize()
+            ) {
+                items(
+                    count = images.itemCount,
+                    key = images.itemKey { it.id }
+                ) { index ->
+                    val item = images[index]
+                    if (item != null) {
+                        AsyncImage(
+                            model = item.sizes.first().url,
+                            contentDescription = null,
+                            contentScale = ContentScale.FillBounds,
+                            placeholder = painterResource(R.drawable.no_photo),
+                            modifier = Modifier
+                                .height(125.dp)
+                                .clickable(onClick = { onImageClick(item.id) })
+                        )
+                    }
                 }
+
+                loadState(images.loadState.append, images::retry)
             }
-
-            loadState(images.loadState.append, images::retry)
         }
-
     }
 
 }
--- app/src/main/java/com/example/nanopost/presentation/profilePostsScreen/ProfilePostsScreen.kt
+++ app/src/main/java/com/example/nanopost/presentation/profilePostsScreen/ProfilePostsScreen.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import com.example.nanopost.R
@@ -31,6 +35,7 @@ fun ProfilePostsScreen(
     onBackClick: () -> Unit,
 ) {
     val posts = profilePostsViewModel.posts.collectAsLazyPagingItems()
+    val pullToRefreshState = rememberPullToRefreshState()
 
     Scaffold(
         topBar = {
@@ -49,42 +54,49 @@ fun ProfilePostsScreen(
             )
         }
     ) { paddingValues ->
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            contentPadding = PaddingValues(
-                bottom = paddingValues.calculateBottomPadding() + 16.dp,
-                top = paddingValues.calculateTopPadding() + 16.dp,
-                start = paddingValues.calculateStartPadding(LayoutDirection.Ltr),
-                end = paddingValues.calculateEndPadding(LayoutDirection.Ltr)
-            ),
-            modifier = Modifier.padding(horizontal = 16.dp)
+        PullToRefreshBox(
+            isRefreshing = posts.loadState.refresh is LoadState.Loading,
+            onRefresh = posts::refresh,
+            state = pullToRefreshState,
+            modifier = Modifier.fillMaxSize()
         ) {
-            items(
-                count = posts.itemCount,
-                key = posts.itemKey { it.id }
-            ) { index ->
-                val item = posts[index]
-                if (item != null) {
-                    PostListItem(
-                        post = item,
-                        onClick = onPostClick,
-                        onImageClick = onImageClick,
-                        onProfileClick = {},
-                        onLikeClick = {
-                            if (!isUserProfile){
-                                profilePostsViewModel.likePost(it)
-                            }
-                        },
-                        onUnlikeClick = {
-                            if (!isUserProfile){
-                                profilePostsViewModel.unlikePost(it)
-                            }
-                        },
-                    )
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(
+                    bottom = paddingValues.calculateBottomPadding() + 16.dp,
+                    top = paddingValues.calculateTopPadding() + 16.dp,
+                    start = paddingValues.calculateStartPadding(LayoutDirection.Ltr),
+                    end = paddingValues.calculateEndPadding(LayoutDirection.Ltr)
+                ),
+                modifier = Modifier.padding(horizontal = 16.dp).fillMaxSize()
+            ) {
+                items(
+                    count = posts.itemCount,
+                    key = posts.itemKey { it.id }
+                ) { index ->
+                    val item = posts[index]
+                    if (item != null) {
+                        PostListItem(
+                            post = item,
+                            onClick = onPostClick,
+                            onImageClick = onImageClick,
+                            onProfileClick = {},
+                            onLikeClick = {
+                                if (!isUserProfile) {
+                                    profilePostsViewModel.likePost(it)
+                                }
+                            },
+                            onUnlikeClick = {
+                                if (!isUserProfile) {
+                                    profilePostsViewModel.unlikePost(it)
+                                }
+                            },
+                        )
+                    }
                 }
-            }
 
-            loadState(posts.loadState.append, onRetryClick = posts::retry)
+                loadState(posts.loadState.append, onRetryClick = posts::retry)
+            }
         }
     }
 }
